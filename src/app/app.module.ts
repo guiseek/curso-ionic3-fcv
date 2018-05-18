c3d6bcee48cdbbdf59b0ec33feac16dd159beff8
Guilherme Siquinelli <guiseek@MacBook-Pro-de-Guilherme.local>
2018-05-17 23:41:14 -0300

    Projeto base para iniciar o curso

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { environment } from '../environments/environment';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDvaar_7Tz4Xc4h9JH0JYP_UKvZwfy_t9g",
+    authDomain: "fcv-chat.firebaseapp.com",
+    databaseURL: "https://fcv-chat.firebaseio.com",
+    projectId: "fcv-chat",
+    storageBucket: "",
+    messagingSenderId: "701220424157"
+    // apiKey: '<your-key>',
+    // authDomain: '<your-project-authdomain>',
+    // databaseURL: '<your-database-URL>',
+    // projectId: '<your-project-id>',
+    // storageBucket: '<your-storage-bucket>',
+    // messagingSenderId: '<your-messaging-sender-id>'
+  }
+};
